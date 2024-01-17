2c899ca1e2ff13fbeea8c9b3c760c144c13e0f2a
masha kulakova <mashakulakova04@inbox.ru>
2024-01-18 00:02:53 +0300

    no comments

--- main.js
+++ main.js
@@ -3,31 +3,31 @@ function getRouts(){
     let xhr = new XMLHttpRequest();
     let url = "http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=2ca053e5-b869-454d-ada8-8b9b35315db5";
     xhr.open("GET", url, true);
-    xhr.send();           //Отправка запроса
-    xhr.onload = function() {        //Обработка ответа сервера
-        if (xhr.status === 200) {         //Обработка успешного ответа
+    xhr.send();       
+    xhr.onload = function() {       
+        if (xhr.status === 200) {        
             let routes = JSON.parse(xhr.responseText); 
-            displayRoutes(routes);  // Вызов функции для отображения маршрутов в таблице
+            displayRoutes(routes);  
         } else {
-            console.error('Ошибка', xhr.status);   //Обработка ошибок
+            console.error('Ошибка', xhr.status);   
         }
     };
-    xhr.onerror = function() {     //Обработчик события ошибки при отправке запроса
+    xhr.onerror = function() {     
         console.error('Ошибка');
     };
 }
 
-function displayRoutes(routes) {           // Вывод информации на страницу
-   let tableBody = document.getElementById('routes');     //Получение ссылки на элемент в html
-   tableBody.innerHTML = "";                    //Очистка от старых данных в таблице
-   for (let i = 0; i < routes.length; i++) {      //Цикл для перебора массива routes и добавления данных в таблицу 
-    tableBody.innerHTML += `<tr><td>${routes[i].name}</td>` +         //Добавление HTML-кода внутри элемента с id
+function displayRoutes(routes) {           
+   let tableBody = document.getElementById('routes');     
+   tableBody.innerHTML = "";                    
+   for (let i = 0; i < routes.length; i++) {       
+    tableBody.innerHTML += `<tr><td>${routes[i].name}</td>` +         
         `<td>${routes[i].description}</td>` +
         `<td>${routes[i].mainObject}</td>` +
         `<td><button>Выбрать</button></td>`;
     }
 }
 
-window.onload = (e) => {     //Обработчик события для окна, вызывающий функцию getRoutes
+window.onload = (e) => {     
     getRouts();
 };
